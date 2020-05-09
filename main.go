d7598caa18da00a545da221d4619acca7f26fc41
Denis Babochenko <babochenko.denis@yandex.ru>
2020-05-09 21:46:04 +0300

    init main.go

--- main.go
+++ main.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"github.com/docker/cli/cli-plugins/manager"
 	"github.com/docker/cli/cli-plugins/plugin"
 	"github.com/docker/cli/cli/command"
+	"github.com/docker/docker/api/types"
 	"github.com/spf13/cobra"
+	"os"
 )
 
+var metadata = manager.Metadata{
+	SchemaVersion: "0.1.0",
+	Vendor:        "dbabochenko",
+	Version:       "0.1.0",
+}
+
+func createPluginCommand(cli command.Cli) *cobra.Command {
+	return &cobra.Command{
+		Use:     "script",
+		Example: "script .",
+		Args:    nil,
+		Run: func(cmd *cobra.Command, args []string) {
+			client := cli.Client()
+			// result := args[0]
+
+			resp, err := client.ImageBuild(nil, nil, types.ImageBuildOptions{})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Could not build image: %s", err)
+				return
+			}
+
+			var response []byte
+			_, _ = resp.Body.Read(response)
+
+			container := ""
+
+			err = client.ContainerStart(nil, container, types.ContainerStartOptions{})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Could not start container: %s", err)
+				return
+			}
+		},
+	}
+}
+
 func main() {
-	metadata := manager.Metadata{
-		SchemaVersion: "0.1.0",
-		Vendor:        "dbabochenko",
-		Version:       "0.1.0",
-	}
-
-	cmd := func(cli command.Cli) *cobra.Command {
-		command := cobra.Command{}
-
-		return &command
-	}
-
-	plugin.Run(cmd, metadata)
+	plugin.Run(createPluginCommand, metadata)
 }
