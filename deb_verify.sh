1dcb32f5a613f1d9c8d1300d92c3659f632454c6
Denis Babochenko <babochenko.denis@yandex.ru>
2020-05-27 23:44:11 +0300

    [ci] preinstall linter #3

--- deb_verify.sh
+++ deb_verify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
->/dev/null command -p lintian || yes | apt-get install lintian
+>/dev/null command -p lintian || yes | sudo apt-get install lintian
 
 lintian --info ./build/dockerscript_1_all.deb
