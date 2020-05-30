6e61b8933676abe64c214ceb20e7f91846340222
Denis Babochenko <babochenko.denis@yandex.ru>
2020-05-30 21:16:44 +0300

    pre-install docker before dockerscript installation

--- deb_install.sh
+++ deb_install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 package="$(find ./build/ -name '*.deb')"
 
+>/dev/null command -p docker || {
+    yes | sudo apt-get install docker
+}
+
 if [[ ! -f "$package" ]]; then
     echo 'could not find package to install! exiting...'
     exit 1
@@ -9,4 +13,4 @@ fi
 echo 'installing package...'
 sudo dpkg -i "$package" \
     && echo 'package installed!' \
-    || echo "can't install package! exiting..."
+    || { echo "can't install package! exiting..."; exit 1; }
