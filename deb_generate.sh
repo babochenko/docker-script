b629124d42dd3893e156139565619391714582dd
Denis Babochenko <babochenko.denis@yandex.ru>
2020-06-03 00:12:36 +0300

    use git buildpackage instead of bzr

--- deb_generate.sh
+++ deb_generate.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-entry_dir="$(pwd)"
-cleanup (){
-    cd "$entry_dir"
-}
-trap cleanup EXIT
-
-cd bin/
-bzr builddeb --result-dir=../build -- -nc -us -uc
+gbp buildpackage --git-export-dir='./build'
